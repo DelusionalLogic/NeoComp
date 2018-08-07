28ad1728917c5b4e9e2ff71a2b7ef52200f05e5c
Jesper Jensen <jesper@slashwin.dk>
2018-08-07 20:52:23 +0200

    Fix indentation error

--- src/swiss.h
+++ src/swiss.h
@@ -32,7 +32,7 @@ struct MetaComponent {
 
 enum ComponentType {
     COMPONENT_META, // Special component used for bookkeeping
-	COMPONENT_END = COMPONENT_META,
+    COMPONENT_END = COMPONENT_META,
     COMPONENT_MUD, // The goal is to eliminate this one
     COMPONENT_SHADOW, // Right now this is only used for testing
     COMPONENT_OPACITY,
