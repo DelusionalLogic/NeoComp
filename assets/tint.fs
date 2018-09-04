16384586a95556e179ae2947122c4fafd6c73b06
Jesper Jensen <jesper@slashwin.dk>
2018-09-04 23:23:21 +0200

    Slightly better noise

--- assets/tint.fs
+++ assets/tint.fs
@@ -8,13 +8,13 @@ uniform vec3 color;
 uniform float opacity;
 
 float rand(in vec2 co){
-    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
+    return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);
 }
 
-void main() {
+void main(void){
     vec2 uv = fragmentUV;
     gl_FragColor = vec4(color, 1);
     gl_FragColor *= opacity;
     vec2 screen_uv = gl_FragCoord.xy / viewport;
-    gl_FragColor.rgb += vec3(rand(screen_uv) * .05);
+    gl_FragColor.rgb *= vec3(1 - ((rand(screen_uv) - .5) * .2));
 }
