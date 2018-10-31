d45a79c026c8cf87eef0b6392941c84facc8ae31
BoAnd <3335069+BoAnd@users.noreply.github.com>
2018-10-31 17:24:32 +0100

    Missing spaces.
    To keep the code consistent across the entire codebase you should include a space here. It's not that difficult.

--- assets/tint.fs
+++ assets/tint.fs
@@ -7,11 +7,11 @@ uniform vec2 viewport;
 uniform vec3 color;
 uniform float opacity;
 
-float rand(in vec2 co){
+float rand(in vec2 co) {
     return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);
 }
 
-void main(void){
+void main(void) {
     vec2 screen_uv = gl_FragCoord.xy / viewport;
     if(rand(screen_uv) < .85)
         discard;
