3ce262a98b374cbf93349db97d0d78f6dc3e5cda
Jesper Jensen <jesper@slashwin.dk>
2021-02-14 18:40:12 +0100

    Move drawable stuff into texture component

--- src/systems/texture.c
+++ src/systems/texture.c
@@ -154,9 +154,40 @@ static void update_window_textures(Swiss* em, struct X11Context* xcontext) {
     zone_leave(&ZONE_x_communication);
 }
 
+static void delete_textures(Swiss* em, enum ComponentType* query) {
+    for_componentsArr(it, em, query) {
+        struct BindsTextureComponent* b = swiss_getComponent(em, COMPONENT_BINDS_TEXTURE, it.id);
+
+        wd_delete(&b->drawable);
+    }
+    swiss_removeComponentWhere(
+        em,
+        COMPONENT_BINDS_TEXTURE,
+        query
+    );
+}
 
 void texturesystem_tick(Swiss* em, struct X11Context* xcontext) {
     zone_scope(&ZONE_texture_tick);
+
+    // Mapping a window causes it to bind from X
+    for_components(it, em,
+            COMPONENT_MAP, COMPONENT_TRACKS_WINDOW, COMPONENT_REDIRECTED, CQ_NOT, COMPONENT_BINDS_TEXTURE, CQ_END) {
+        struct TracksWindowComponent* tracksWindow = swiss_getComponent(em, COMPONENT_TRACKS_WINDOW, it.id);
+        struct BindsTextureComponent* bindsTexture = swiss_addComponent(em, COMPONENT_BINDS_TEXTURE, it.id);
+
+        if(!wd_init(&bindsTexture->drawable, xcontext, tracksWindow->id)) {
+            printf_errf("Failed initializing window drawable on map");
+        }
+    }
+
+    delete_textures(em, (enum ComponentType[]) {
+        COMPONENT_BINDS_TEXTURE, COMPONENT_UNMAP, CQ_END
+    });
+    delete_textures(em, (enum ComponentType[]) {
+        COMPONENT_BINDS_TEXTURE, COMPONENT_BYPASS, CQ_END
+    });
+
     // Resize textures when mapping a window with a texture
     for_components(it, em,
             COMPONENT_MAP, COMPONENT_PHYSICAL, COMPONENT_REDIRECTED, COMPONENT_TEXTURED, CQ_END) {
--- src/systems/xorg.c
+++ src/systems/xorg.c
@@ -5,8 +5,6 @@
 #include "logging.h"
 #include "window.h"
 
-DECLARE_ZONE(fetch_xprop);
-
 static void unredirect(Swiss* em, struct X11Context* xcontext, enum ComponentType* query) {
     for_componentsArr(it, em, query) {
         struct TracksWindowComponent* tracksWindow = swiss_getComponent(em, COMPONENT_TRACKS_WINDOW, it.id);
@@ -20,19 +18,6 @@ static void unredirect(Swiss* em, struct X11Context* xcontext, enum ComponentTyp
     );
 }
 
-static void delete_textures(Swiss* em, enum ComponentType* query) {
-    for_componentsArr(it, em, query) {
-        struct BindsTextureComponent* b = swiss_getComponent(em, COMPONENT_BINDS_TEXTURE, it.id);
-
-        wd_delete(&b->drawable);
-    }
-    swiss_removeComponentWhere(
-        em,
-        COMPONENT_BINDS_TEXTURE,
-        query
-    );
-}
-
 static void doUnmap(Swiss* em, struct X11Context* xcontext, struct Atoms* atoms) {
     //@PERFORMANCE: Technically all windows that redirect also tracks a window,
     //so we don't really need to query for both
@@ -41,13 +26,6 @@ static void doUnmap(Swiss* em, struct X11Context* xcontext, struct Atoms* atoms)
     });
     unredirect(em, xcontext, (enum ComponentType[]) {
         COMPONENT_BYPASS, COMPONENT_TRACKS_WINDOW, COMPONENT_REDIRECTED, CQ_END
-    });
-
-    delete_textures(em, (enum ComponentType[]) {
-        COMPONENT_BINDS_TEXTURE, COMPONENT_UNMAP, CQ_END
-    });
-    delete_textures(em, (enum ComponentType[]) {
-        COMPONENT_BINDS_TEXTURE, COMPONENT_BYPASS, CQ_END
     });
 }
 
@@ -64,17 +42,6 @@ static void doMap(Swiss* em, struct X11Context* xcontext, struct Atoms* atoms) {
             struct TracksWindowComponent* tracksWindow = swiss_getComponent(em, COMPONENT_TRACKS_WINDOW, it.id);
 
             XCompositeRedirectWindow(xcontext->display, tracksWindow->id, CompositeRedirectManual);
-        }
-    }
-
-    // Mapping a window causes it to bind from X
-    for_components(it, em,
-            COMPONENT_MAP, COMPONENT_TRACKS_WINDOW, COMPONENT_REDIRECTED, CQ_NOT, COMPONENT_BINDS_TEXTURE, CQ_END) {
-        struct TracksWindowComponent* tracksWindow = swiss_getComponent(em, COMPONENT_TRACKS_WINDOW, it.id);
-        struct BindsTextureComponent* bindsTexture = swiss_addComponent(em, COMPONENT_BINDS_TEXTURE, it.id);
-
-        if(!wd_init(&bindsTexture->drawable, xcontext, tracksWindow->id)) {
-            printf_errf("Failed initializing window drawable on map");
         }
     }
 }
